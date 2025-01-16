885950717c7e4b3358c4920a8395ac894294fb77
kanondrago <ronaldchavezr@gmail.com>
2025-01-16 22:41:31 +0100

    Diseño del formulario

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+  import Header from './components/Header.vue'
+  import Formulario from './components/Formulario.vue'
+
+</script>
+
+<template>
+
+  <div class="container mx-auto mt-20">
+    <Header></Header>
+    <div class="mt-12 md:flex">
+      <Formulario></Formulario>
+    </div>
+  </div>
+
+</template>
---
+++ src/components/Formulario.vue
@@ -0,0 +1,101 @@
+
+<script setup>
+
+</script>
+
+<template>
+    <div class="md:w-1/2">
+        <h2 class="font-black text-3xl text-center">Seguimiento de pacientes</h2>
+
+        <p class="text-lg mt-5 text-center mb-10">
+            Añade pacientes y 
+            <span class="text-indigo-600 font-bold">Adminístralos</span>
+        </p>
+
+        <form 
+            action=""
+            class="bg-white shadow-md rounded-lg py-10 px-5 mb-10"
+        >
+            <div class="mb-5">
+                <label 
+                    for="mascota"
+                    class="block text-gray-700 uppercase font-bold"
+                >
+                    Nombre Mascota
+                </label>
+                <input 
+                    id="mascota"
+                    type="text"
+                    placeholder="Nombre de la mascota: "
+                    class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+                >
+            </div>
+
+            <div class="mb-5">
+                <label 
+                    for="propietario"
+                    class="block text-gray-700 uppercase font-bold"
+                >
+                    Nombre del propietario
+                </label>
+                <input 
+                    id="propietario"
+                    type="text"
+                    placeholder="Nombre del propietario(@): "
+                    class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+                >
+            </div>
+
+            <div class="mb-5">
+                <label 
+                    for="email"
+                    class="block text-gray-700 uppercase font-bold"
+                >
+                    Email del propietario
+                </label>
+                <input 
+                    id="email"
+                    type="email"
+                    placeholder="Email del propietario: "
+                    class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+                >
+            </div>
+
+            <div class="mb-5">
+                <label 
+                    for="alta"
+                    class="block text-gray-700 uppercase font-bold"
+                >
+                    Alta
+                </label>
+                <input 
+                    id="alta"
+                    type="date"
+                    class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+                >
+            </div>
+
+            <div class="mb-5">
+                <label 
+                    for="sintomas"
+                    class="block text-gray-700 uppercase font-bold"
+                >
+                    Síntomas
+                </label>
+                <input 
+                    id="sintomas"
+                    placeholder="Escribe los síntomas de tu mascota"
+                    class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md h-40"
+                >
+            </div>
+
+            <input 
+                type="submit"
+                class="bg-indigo-600 w-full p-3 text-white uppercase font-bold hover:bg-indigo-700 cursor-pointer transition-colors rounded-lg"
+                value="Registrar paciente"
+            >
+
+        </form>
+
+    </div>
+</template>
