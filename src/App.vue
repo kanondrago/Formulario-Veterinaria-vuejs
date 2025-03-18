f085271295bdb771b55c2a5ea065a133bfb95407
wolfrit <wolfrit.company@gmail.com>
2025-03-18 21:10:29 +0100

    avance agregado de pacientes

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,12 @@
 
 <script setup>
 
+  import { ref } from 'vue';
+  
   import Header from './components/Header.vue'
   import Formulario from './components/Formulario.vue';
+
+  const pacientes = ref([]);
 
 </script>
 
@@ -12,6 +16,20 @@
 
     <div class="mt-12 md:flex">
       <Formulario></Formulario>
+
+      <div class="md:w-1/2 md:h-screen overflow-y-scroll">
+        <h3 class="font-black text-3xl text-center">Administra tus pacientes</h3>
+
+        <div v-if="pacientes.length > 0">
+
+        </div>
+
+        <div v-else>
+          <h2 class="text-center text-2xl mt-20 font-bold">No hay pacientes</h2>
+        </div>
+
+      </div>
     </div>
+
   </div>
 </template>
